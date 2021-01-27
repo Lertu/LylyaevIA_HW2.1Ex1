2c85dfb9a956e6af3acf8066e1ddccefb4b44a3d
Lertu <Lertu>
2021-01-27 20:50:42 +0300

    Initial Commit

---
+++ LylyaevIA_HW2.1Ex1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LylyaevIA_HW2.1Ex1
+//
+//  Created by Игорь И on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
